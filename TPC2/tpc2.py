5a7bbf318ccd85e0a662f0b2efb33ecee55095ca
andrenunes74 <61511913+andrenunes74@users.noreply.github.com>
2023-02-22 23:09:43 +0000

    tpc2

---
+++ TPC2/tpc2.py
@@ -0,0 +1,26 @@
+import sys
+import re
+
+def main():
+    soma = 0
+    flag = 0
+
+    print("||||          Soma todas as sequências de dígitos que encontra num texto          ||||")
+    print("|||| :q -> sair | Off -> desliga contador | On -> liga contador | = -> soma atual ||||")
+    print("Insira uma entrada de texto:")
+
+    for line in sys.stdin:
+        if re.search(":q",line): break
+        if re.search("Off",line): flag = 1
+        if re.search("On",line): flag = 0
+        if re.search("=",line): print("Soma atual:" + str(soma))
+        if re.search(".*\d+.*",line) and flag == 0:
+            i=0
+            while(i<len(line)):
+                if line[i].isdigit(): soma+=int(line[i])
+                i+=1
+
+    print("Resolvido! Soma:" + str(soma))
+
+if __name__ == "__main__":
+    main()
